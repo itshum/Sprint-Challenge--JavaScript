68246ac0b973ead7b566c2a5a33b7b1e814e02c9
Humayun Rashid <humayun@Humayuns-iMac.local>
2019-12-20 13:25:55 -0500

    Finished Function problems

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, c){
+  return console.log(cb(a, b));
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +17,29 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b){
+  return a+b;
+}
+
+function multiply(a, b){
+  return a*b;
+}
+
+function greeting(first_name, last_name){
+  return `Hello ${first_name} ${last_name}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: nestedFunction is a function inside myFunctiuon. It is accessing properties of myFunction because of closure. Closure gives us the ability to put functions together. And because myFunction is the parent, nestedFunction is inheriting from its parent. Additionally, nested functions allow us to hold on to code, not run it until we need it later. Also, we can't push things up, we can only push things down. Children can only inherit from parents, parent's can't. It's very similar to LESS and CSS concept of nesting styles. 
 
 
 const external = "I'm outside the function";
